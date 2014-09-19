9907c7457d29d2906ecf4f795a0d076541e87014
Niles Madison <nmadison@blackducksoftware.com>
2014-09-19 17:53:52 +0000

    Changed to RestController
    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4881 90649d4e-7a4c-4683-80da-d44f83f08aad

--- src/main/java/com/blackducksoftware/soleng/idcopier/controller/IDCProjectController.java
+++ src/main/java/com/blackducksoftware/soleng/idcopier/controller/IDCProjectController.java
@@ -8,25 +8,19 @@ All rights reserved. **/
  */
 package com.blackducksoftware.soleng.idcopier.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.blackducksoftware.soleng.idcopier.constants.IDCPathConstants;
 import com.blackducksoftware.soleng.idcopier.constants.IDCViewConstants;
 import com.blackducksoftware.soleng.idcopier.constants.IDCViewModelConstants;
-import com.blackducksoftware.soleng.idcopier.model.ProjectModel;
 import com.blackducksoftware.soleng.idcopier.model.IDCSession;
-import com.blackducksoftware.soleng.idcopier.service.LoginService;
 import com.blackducksoftware.soleng.idcopier.service.ProjectService;
 
 /**
@@ -35,28 +29,24 @@ import com.blackducksoftware.soleng.idcopier.service.ProjectService;
  * 
  */
 
-@Controller
-@SessionAttributes(IDCViewModelConstants.IDC_SESSION) 
-public class IDCProjectController
-{
-    static Logger log = Logger.getLogger(IDCProjectController.class);
+@RestController
+@SessionAttributes(IDCViewModelConstants.IDC_SESSION)
+public class IDCProjectController {
+	static Logger log = Logger.getLogger(IDCProjectController.class);
 
-    @RequestMapping(value = IDCPathConstants.PROJECT_DISPLAY_TREE)
-    public ModelAndView processLogin(
-	    @RequestParam(value = IDCViewModelConstants.PROJECT_SOURCE_ID) String projectId,
-	    @ModelAttribute(IDCViewModelConstants.IDC_SESSION) IDCSession session, 
-	    Model model)
-    {
-	ModelAndView modelAndView = new ModelAndView();
+	@RequestMapping(value = IDCPathConstants.PROJECT_DISPLAY_TREE)
+	public ModelAndView processLogin(@RequestParam(value = IDCViewModelConstants.PROJECT_SOURCE_ID) String projectId,
+			@ModelAttribute(IDCViewModelConstants.IDC_SESSION) IDCSession session, Model model) {
+		ModelAndView modelAndView = new ModelAndView();
 
-	log.info("Processing project: " + projectId);
+		log.info("Processing project: " + projectId);
 
-	ProjectService ps = new ProjectService(session);
-	String jsonTree = ps.getProjectJSON(projectId);
+		ProjectService ps = new ProjectService(session);
+		String jsonTree = ps.getProjectJSON(projectId);
 
-	modelAndView.addObject(IDCViewModelConstants.PROJECT_JSON_TREE, jsonTree);
+		modelAndView.addObject(IDCViewModelConstants.PROJECT_JSON_TREE, jsonTree);
 
-	modelAndView.setViewName(IDCViewConstants.PROJECT_PAGE);
-	return modelAndView;
-    }
+		modelAndView.setViewName(IDCViewConstants.PROJECT_PAGE);
+		return modelAndView;
+	}
 }
