3a4d1e51f5cda6c1c4d6ef24b240b1d33519535f
Niles Madison <nmadison@blackducksoftware.com>
2014-09-19 06:33:11 +0000

    Some loading to the UI using RESTful data
    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4867 90649d4e-7a4c-4683-80da-d44f83f08aad

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,8 +1,121 @@
-jQuery(document).ready(function() {
-	$.ajax({
-		url : 'servers',
-		success : function(data) {
-			alert(data);
-		}
-	});
-});
+jQuery(document).ready(
+		function() {
+			var selectSourceServer = "Select Source Server";
+			var selectDestinationServer = "Select Destinaton Server";
+			var selectSourceProject = "Select Source Project";
+			var selectDestinationProject = "Select Destinaton Project";
+
+			/**
+			 * Skipping a bunch in here for now, because we are only working
+			 * with one server. So I am going to just load all of the projects
+			 * to the drop downs and worry about the rest later
+			 * 
+			 * -NM
+			 */
+			$.getJSON('servers',
+					function(data) {
+						if (data.length === 1) {
+							$('.selectSourceServer').empty();
+							$('.selectDestinationServer').empty();
+
+							$.each(data, function(index, value) {
+								$('.selectSourceServer').append(
+										"<option>" + value.ServerName
+												+ "</option>");
+
+								$('.selectDestinationServer').append(
+										"<option>" + value.ServerName
+												+ "</option>");
+							});
+						} else {
+							$('.selectSourceServer').empty();
+							$('.selectDestinationServer').empty();
+
+							$('.selectSourceServer').append(
+									"<option>" + selectSourceServer
+											+ "</option>");
+
+							$('.selectDestinationServer').append(
+									"<option>" + selectDestinationServer
+											+ "</option>");
+
+							$.each(data, function(index, value) {
+								$('.selectSourceServer').append(
+										"<option>" + value.ServerName
+												+ "</option>");
+
+								$('.selectDestinationServer').append(
+										"<option>" + value.ServerName
+												+ "</option>");
+							});
+						}
+					});
+
+			/**
+			 * Set the source projects
+			 */
+			$.getJSON('sourceProjects', function(data) {
+				$('.selectSourceProject').empty();
+
+				$('.selectSourceProject').append(
+						"<option>" + selectSourceProject + "</option>");
+
+				$.each(data, function(index, value) {
+					$('.selectSourceProject').append(
+							"<option id=\"" + value.projectId + "\">"
+									+ value.name + "</option>");
+				});
+
+			});
+
+			/**
+			 * Set the destination projects
+			 */
+			$.getJSON('destinationProjects', function(data) {
+				$('.selectDestinationProject').empty();
+
+				$('.selectDestinationProject').append(
+						"<option>" + selectDestinationProject + "</option>");
+
+				$.each(data, function(index, value) {
+					$('.selectDestinationProject').append(
+							"<option id=\"" + value.projectId + "\">"
+									+ value.name + "</option>");
+				});
+			});
+
+			$(".selectSourceServer").change(function() {
+				// Wont do anything since we're only working with one
+				// server so no change will happen
+				if (this.value !== selectSourceServer) {
+					console.log("source server = " + this.value);
+				}
+			});
+
+			$(".selectDestinationServer").change(function() {
+				// Wont do anything since we're only working with one
+				// server so no change will happen
+				if (this.value !== selectDestinationServer) {
+					console.log("destination server = " + this.value);
+				}
+			});
+
+			$(".selectSourceProject").change(function() {
+				if (this.value !== selectDestinationProject) {
+					console.log("source project = " + this.value);
+				}
+			});
+
+			$(".selectDestinationProject").change(function() {
+				if (this.value !== selectDestinationProject) {
+					console.log("destination project = " + this.value);
+				}
+			});
+		});
+
+
+/*
+ * jQuery(document).ready(function() { $.ajax({ url : 'servers', success :
+ * function(data) { alert(data[1]); // $("#selectSourceServer").append($("<option></option>").attr("value", //
+ * data[0].ServerName).text(server.ServerName); } }); });
+ */
