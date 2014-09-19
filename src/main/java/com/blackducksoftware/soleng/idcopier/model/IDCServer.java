dd495eaf0b79099433cd8f69c985684b006743b9
Niles Madison <nmadison@blackducksoftware.com>
2014-09-19 06:28:11 +0000

    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4866 90649d4e-7a4c-4683-80da-d44f83f08aad

---
+++ src/main/java/com/blackducksoftware/soleng/idcopier/model/IDCServer.java
@@ -0,0 +1,13 @@
+package com.blackducksoftware.soleng.idcopier.model;
+
+public class IDCServer {
+	private String ServerName;
+
+	public IDCServer(String server) {
+		this.ServerName = server;
+	}
+
+	public String getServerName() {
+		return ServerName;
+	}
+}
