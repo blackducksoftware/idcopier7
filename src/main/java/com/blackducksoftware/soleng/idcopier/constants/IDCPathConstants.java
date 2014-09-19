aa4ec658ebde24f6324ea6e48afb2875071118d7
Niles Madison <nmadison@blackducksoftware.com>
2014-09-19 15:56:28 +0000

    Updated the Constants for the paths
    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4875 90649d4e-7a4c-4683-80da-d44f83f08aad

--- src/main/java/com/blackducksoftware/soleng/idcopier/constants/IDCPathConstants.java
+++ src/main/java/com/blackducksoftware/soleng/idcopier/constants/IDCPathConstants.java
@@ -9,24 +9,30 @@ All rights reserved. **/
 package com.blackducksoftware.soleng.idcopier.constants;
 
 /**
- *  List of all the URLs referenced within controllers
+ * List of all the URLs referenced within controllers
  * 
- *  @author Ari Kamen
- *  @date Sep 17, 2014
+ * @author Ari Kamen
+ * @date Sep 17, 2014
  *
  */
-public class IDCPathConstants
-{
+public class IDCPathConstants {
 
-    /**
-     * Login
-     */
-    public static final String LOGIN_MAIN_PATH = "/login";
-    public static final String LOGIN_PROCESS_PATH = "/login.do";
-    public static final String LOGIN_REDIRECT = "/redirect";
-    
-    /**
-     * Projects
-     */
-    public static final String PROJECT_DISPLAY_TREE = "/processProject.do";
+	/**
+	 * Login
+	 */
+	public static final String LOGIN_MAIN_PATH = "/login";
+	public static final String LOGIN_PROCESS_PATH = "/login.do";
+	public static final String LOGIN_REDIRECT = "/redirect";
+
+	/**
+	 * Projects
+	 */
+	public static final String PROJECT_DISPLAY_TREE = "/processProject.do";
+
+	/**
+	 * RESTful
+	 */
+	public static final String SERVERS = "/servers";
+	public static final String SOURCE_PROJECTS = "/sourceProjects";
+	public static final String DESTINATION_PROJECTS = "/destinationProjects";
 }
