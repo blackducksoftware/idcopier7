144bd4b97720dd105c7550256a726145e4ffaf78
Niles Madison <nmadison@blackducksoftware.com>
2014-09-29 07:11:46 +0000

    NI-39 Users can now view the active log as they are using the utility.
    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4957 90649d4e-7a4c-4683-80da-d44f83f08aad

--- src/main/java/com/blackducksoftware/soleng/idcopier/service/LoginService.java
+++ src/main/java/com/blackducksoftware/soleng/idcopier/service/LoginService.java
@@ -22,140 +22,119 @@ import com.blackducksoftware.soleng.idcopier.model.IDCSession;
  * @date Sep 15, 2014
  * 
  */
-public class LoginService
-{
-    static Logger log = Logger.getLogger(LoginService.class);
+public class LoginService {
+	static Logger log = Logger.getLogger(LoginService.class);
 
-    private boolean loggedIn = false;
+	private boolean loggedIn = false;
 
-    // Map of servers by their names, where names are hostnames
-    private HashMap<String, IDCServer> serverMap = new HashMap<String, IDCServer>();
-    // Map of established proxies, a proxy is established upon a successful
+	// Map of servers by their names, where names are hostnames
+	private HashMap<String, IDCServer> serverMap = new HashMap<String, IDCServer>();
+	// Map of established proxies, a proxy is established upon a successful
 
-    private HashMap<String, ProtexServerProxy> proxyMap = new HashMap<String, ProtexServerProxy>();
+	private HashMap<String, ProtexServerProxy> proxyMap = new HashMap<String, ProtexServerProxy>();
 
-    public LoginService()
-    {
+	public LoginService() {
 
-    }
-
-    /**
-     * The first login
-     * @param session
-     */
-    public void loginSession(IDCSession session) throws Exception
-    {
-	try
-	{
-	    ProtexServerProxy proxy = new ProtexServerProxy(session.getServerURI(),
-		    session.getUserName(), session.getPassword());
-
-	    proxy.validateCredentials();
-
-	    log.info("Login successful");
-	    proxy.close();
-	} catch (Exception e)
-	{
-	    log.error("Error logging in: " + e.getMessage());
-	    throw new Exception(e.getCause().getMessage());
-	}
-    }
-    
-    
-    public IDCServer getServerByName(String serverName) throws Exception
-    {
-	IDCServer server = serverMap.get(serverName);
-	if(server == null)
-	    throw new Exception("Unable to find server with name: " + serverName);
-	return server;
-    }
-    
-    /**
-     * Retrieves a connection for that server
-     * 
-     * @param serverName
-     * @return
-     * @throws Exception
-     */
-    public ProtexServerProxy getProxy(String serverName) throws Exception
-    {
-	ProtexServerProxy proxy = null;
-	IDCServer server = serverMap.get(serverName);
-	if (server == null)
-	    throw new Exception("Unable to determine IDCServer with name: "
-		    + serverName);
-
-	proxy = proxyMap.get(serverName);
-	if (proxy == null)
-	{
-	    proxy = getProxy(server);
 	}
 
-	return proxy;
-    }
+	/**
+	 * The first login
+	 * 
+	 * @param session
+	 */
+	public void loginSession(IDCSession session) throws Exception {
+		try {
+			ProtexServerProxy proxy = new ProtexServerProxy(session.getServerURI(), session.getUserName(), session.getPassword());
 
-    /**
-     * Retrieves a proxy object by URI
-     * 
-     * @param serverURI
-     * @return
-     * @throws Exception
-     */
-    public ProtexServerProxy getProxyByServerURI(String serverURI)
-	    throws Exception
-    {
-	IDCServer ourServer = null;
-	ProtexServerProxy proxy = null;
-	String hostName = IDCServer.getHostFromURI(serverURI);
-	ourServer = serverMap.get(hostName);
-	
-	if(ourServer == null)
-	    throw new Exception("Could not find server with name: " + ourServer);
-	proxy = proxyMap.get(ourServer.getServerName());
-	if (proxy == null)
-	{
-	    proxy = getProxy(ourServer);
+			proxy.validateCredentials();
+
+			log.info("Login successful");
+			proxy.close();
+		} catch (Exception e) {
+			log.error("Error logging in: " + e.getMessage());
+			throw new Exception(e.getCause().getMessage());
+		}
 	}
 
-	return proxy;
-    }
-
-    /**
-     * This provides the list of server objects for the service to manage
-     * Populates the internal map for future lookup
-     */
-    public void setServers(List<IDCServer> servers)
-    {
-	for (IDCServer server : servers)
-	{
-	    serverMap.put(server.getServerName(), server);
-	}
-    }
-
-    public ProtexServerProxy getProxy(IDCServer server)
-    {
-	ProtexServerProxy proxy = null;
-	try
-	{
-	    proxy = new ProtexServerProxy(server.getServerURI(),
-		    server.getUserName(), server.getPassword());
-
-	    proxy.validateCredentials();
-
-	    server.setLoggedIn(true);
-	    log.info("Login successful");
-
-	} catch (Exception e)
-	{
-	    log.error("Error logging in: " + e.getMessage());
-	    server.setLoginError(e.getCause().getMessage());
+	public IDCServer getServerByName(String serverName) throws Exception {
+		IDCServer server = serverMap.get(serverName);
+		if (server == null)
+			throw new Exception("Unable to find server with name: " + serverName);
+		return server;
 	}
 
-	return proxy;
-    }
+	/**
+	 * Retrieves a connection for that server
+	 * 
+	 * @param serverName
+	 * @return
+	 * @throws Exception
+	 */
+	public ProtexServerProxy getProxy(String serverName) throws Exception {
+		ProtexServerProxy proxy = null;
+		IDCServer server = serverMap.get(serverName);
+		if (server == null)
+			throw new Exception("Unable to determine IDCServer with name: " + serverName);
 
-    public Boolean isLoggedIn()
-    {
-	return loggedIn;
-    }
+		proxy = proxyMap.get(serverName);
+		if (proxy == null) {
+			proxy = getProxy(server);
+		}
+
+		return proxy;
+	}
+
+	/**
+	 * Retrieves a proxy object by URI
+	 * 
+	 * @param serverURI
+	 * @return
+	 * @throws Exception
+	 */
+	public ProtexServerProxy getProxyByServerURI(String serverURI) throws Exception {
+		IDCServer ourServer = null;
+		ProtexServerProxy proxy = null;
+		String hostName = IDCServer.getHostFromURI(serverURI);
+		ourServer = serverMap.get(hostName);
+
+		if (ourServer == null)
+			throw new Exception("Could not find server with name: " + ourServer);
+		proxy = proxyMap.get(ourServer.getServerName());
+		if (proxy == null) {
+			proxy = getProxy(ourServer);
+		}
+
+		return proxy;
+	}
+
+	/**
+	 * This provides the list of server objects for the service to manage Populates the internal map for future lookup
+	 */
+	public void setServers(List<IDCServer> servers) {
+		for (IDCServer server : servers) {
+			serverMap.put(server.getServerName(), server);
+		}
+	}
+
+	public ProtexServerProxy getProxy(IDCServer server) {
+		ProtexServerProxy proxy = null;
+		try {
+			proxy = new ProtexServerProxy(server.getServerURI(), server.getUserName(), server.getPassword());
+
+			proxy.validateCredentials();
+
+			server.setLoggedIn(true);
+			log.info("Login successful");
+
+		} catch (Exception e) {
+			log.error("Error logging in: " + e.getMessage());
+			server.setLoginError(e.getCause().getMessage());
+		}
+
+		return proxy;
+	}
+
+	public Boolean isLoggedIn() {
+		return loggedIn;
+	}
 }
