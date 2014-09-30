b1030ce65f54a3f3461789154f9af6f0dc753cc3
Niles Madison <nmadison@blackducksoftware.com>
2014-09-30 20:48:45 +0000

    git-svn-id: svn+ssh://svn.blackducksoftware.com/srv/svn/proserv/branches/IDCopier7@4987 90649d4e-7a4c-4683-80da-d44f83f08aad

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -1,45 +1,73 @@
 jQuery(document).ready(
-		function() {
-			var serversJsonURI = "servers";
-			var servers;
+	function () {
+	var serversJsonURI = "servers";
+	var servers;
 
-			$.getJSON(serversJsonURI, function(data) {
-				servers = data;
+	$.getJSON(serversJsonURI, function (data) {
+		servers = data;
 
-				var dataSet = [];
+		var dataSet = [];
 
-				$.each(data, function(index, value) {
-					dataSet.push([ value.serverName, value.userName,
-							getPasswordChars(value.password) ]);
+		$.each(data, function (index, value) {
+			dataSet.push([value.serverName, value.userName,
+					getPasswordChars(value.password)]);
 
-				});
-
-				console.log(dataSet);
-				buildDataTable(dataSet);
-			});
-
-			function buildDataTable(dataSet) {
-				$('#serverConfigurationTable').dataTable({
-					"data" : dataSet,
-					"paging" : false,
-					"bInfo" : false,
-					"columns" : [ {
-						"title" : "Server"
-					}, {
-						"title" : "Username",
-						"class" : "center"
-					}, {
-						"title" : "Password",
-						"class" : "center"
-					} ]
-				});
-			}
-
-			function getPasswordChars(pw) {
-				var out = '';
-				for ( var a = 0; a < pw.length; a++) {
-					out = out + '*';
-				}
-				return out;
-			}
 		});
+
+		buildDataTable(dataSet);
+	});
+
+	$('#serverConfigurationTable').on(
+		'click',
+		'tr',
+		function () {
+		var selectedRow = $('#serverConfigurationTable')
+			.DataTable().row(this).data();
+		var selectedServerName = selectedRow[0];
+
+		var selectedServer;
+
+		for (var a = 0; a < servers.length; a++) {
+			if (selectedServerName === servers[a].serverName) {
+				selectedServer = servers[a];
+			}
+		}
+
+		var serverURI = selectedServer.serverURI;
+		var serverName = selectedServer.serverName;
+		var userName = selectedServer.userName;
+		var password = selectedServer.password;
+
+		console.log(' > ' + selectedServer);
+		$('#serverNameHeader').text(selectedServer.serverName);
+		$('#serverURI').val(serverURI);
+		$('#userName').val(userName);
+		$('#password').val(password);
+		$('#cPassword').val(password);
+	});
+
+	function buildDataTable(dataSet) {
+		$('#serverConfigurationTable').dataTable({
+			"paging" : false,
+			"bInfo" : false,
+			"data" : dataSet,
+			"columns" : [{
+					"title" : "Server"
+				}, {
+					"title" : "Username"
+				}, {
+					"title" : "Password",
+					"width" : "10%"
+				}
+			]
+		});
+	}
+
+	function getPasswordChars(pw) {
+		var out = '';
+		for (var a = 0; a < pw.length; a++) {
+			out = out + '*';
+		}
+		return out;
+	}
+});
