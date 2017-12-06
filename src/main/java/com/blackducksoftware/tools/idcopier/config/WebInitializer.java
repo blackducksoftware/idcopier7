881baa219757f3622dd1c3cb6d0a2d92dbcac01a
nilesmadison <niles.madison@gmail.com>
2017-12-06 16:20:58 -0500

    IDCOPIER-73: Removed blackduck-cxf-utilities from the POM which resolved the CXF/Spring issue that I was hitting.
    Protex is up to date (7.7.2), as is Spring (5.0.2.RELEASE).

--- src/main/java/com/blackducksoftware/tools/idcopier/config/WebInitializer.java
+++ src/main/java/com/blackducksoftware/tools/idcopier/config/WebInitializer.java
@@ -23,41 +23,38 @@
  */
 package com.blackducksoftware.tools.idcopier.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
-
 import com.blackducksoftware.tools.idcopier.constants.IDCViewModelConstants;
+import com.blackducksoftware.tools.idcopier.model.IDCConfig;
 import com.blackducksoftware.tools.idcopier.service.ProjectService;
 import org.apache.log4j.Logger;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import com.blackducksoftware.tools.idcopier.model.IDCConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
 
 public class WebInitializer implements WebApplicationInitializer {
 
-	static Logger log = Logger.getLogger(WebInitializer.class);
+    static Logger log = Logger.getLogger(WebInitializer.class);
 
-	public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) throws ServletException {
 
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.register(IDCWebConfig.class);
-		ctx.register(IDCConfig.class);
-		// ctx.register(LoginService.class);
-		ctx.register(ProjectService.class);
-		ctx.setServletContext(servletContext);
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(IDCWebConfig.class);
+        ctx.register(IDCConfig.class);
+        // ctx.register(LoginService.class);
+        ctx.register(ProjectService.class);
+        ctx.setServletContext(servletContext);
 
-		Dynamic servlet = servletContext.addServlet(IDCViewModelConstants.IDC_WEB_CONTEXT, new DispatcherServlet(ctx));
-		servlet.addMapping("/");
-		servlet.setLoadOnStartup(1);
+        Dynamic servlet = servletContext.addServlet(IDCViewModelConstants.IDC_WEB_CONTEXT, new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
 
-		// Session stuff
-		servletContext.addListener(new IDCSessionListener());
-		IDCSessionFilter sessionFilter = new IDCSessionFilter();
-		servletContext.addFilter("sessionFilter", sessionFilter).addMappingForUrlPatterns(null, false, "/*");
-
-	}
-
+        // Session stuff
+        servletContext.addListener(new IDCSessionListener());
+        IDCSessionFilter sessionFilter = new IDCSessionFilter();
+        servletContext.addFilter("sessionFilter", sessionFilter).addMappingForUrlPatterns(null, false, "/*");
+    }
 }
--- src/main/java/com/blackducksoftware/tools/idcopier/service/LoginService.java
+++ src/main/java/com/blackducksoftware/tools/idcopier/service/LoginService.java
@@ -26,127 +26,125 @@
  */
 package com.blackducksoftware.tools.idcopier.service;
 
+import com.blackducksoftware.sdk.protex.client.util.ProtexServerProxy;
+import com.blackducksoftware.tools.idcopier.model.IDCServer;
+import org.apache.log4j.Logger;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-
-import com.blackducksoftware.sdk.protex.client.util.ProtexServerProxy;
-import com.blackducksoftware.tools.idcopier.model.IDCServer;
-
 /**
  * @author Ari Kamen
  * @date Sep 15, 2014
- *
  */
 
 public class LoginService implements Serializable {
-	static Logger log = Logger.getLogger(LoginService.class);
+    static Logger log = Logger.getLogger(LoginService.class);
 
-	private boolean loggedIn = false;
+    private boolean loggedIn = false;
 
-	// Map of servers by their names, where names are hostnames
-	private HashMap<String, IDCServer> serverMap = new HashMap<String, IDCServer>();
-	// Map of established proxies, a proxy is established upon a successful
-	private HashMap<String, ProtexServerProxy> proxyMap = new HashMap<String, ProtexServerProxy>();
+    // Map of servers by their names, where names are hostnames
+    private HashMap<String, IDCServer> serverMap = new HashMap<String, IDCServer>();
+    // Map of established proxies, a proxy is established upon a successful
+    private HashMap<String, ProtexServerProxy> proxyMap = new HashMap<String, ProtexServerProxy>();
 
-	public LoginService() {
-		serverMap = new HashMap<String, IDCServer>();
-		proxyMap = new HashMap<String, ProtexServerProxy>();
-	}
+    public LoginService() {
+        serverMap = new HashMap<String, IDCServer>();
+        proxyMap = new HashMap<String, ProtexServerProxy>();
+    }
 
-	/**
-	 * The first login
-	 *
-	 * @param serverName
-	 */
-	public IDCServer getServerByName(String serverName) throws Exception {
-		IDCServer server = serverMap.get(serverName);
-		if (server == null)
-			throw new Exception("Unable to find server with name: " + serverName);
-		return server;
-	}
+    /**
+     * The first login
+     *
+     * @param serverName
+     */
+    public IDCServer getServerByName(String serverName) throws Exception {
+        IDCServer server = serverMap.get(serverName);
+        if (server == null)
+            throw new Exception("Unable to find server with name: " + serverName);
+        return server;
+    }
 
-	/**
-	 * Retrieves a connection for that server
-	 *
-	 * @param serverName
-	 * @return
-	 * @throws Exception
-	 */
-	public ProtexServerProxy getProxy(String serverName) throws Exception {
-		ProtexServerProxy proxy = null;
-		IDCServer server = serverMap.get(serverName);
-		if (server == null)
-			throw new Exception("Unable to determine IDCServer with name: " + serverName);
+    /**
+     * Retrieves a connection for that server
+     *
+     * @param serverName
+     * @return
+     * @throws Exception
+     */
+    public ProtexServerProxy getProxy(String serverName) throws Exception {
+        ProtexServerProxy proxy = null;
+        IDCServer server = serverMap.get(serverName);
+        if (server == null)
+            throw new Exception("Unable to determine IDCServer with name: " + serverName);
 
-		proxy = proxyMap.get(serverName);
-		if (proxy == null) {
-			proxy = createProxy(server);
-			proxyMap.put(serverName, proxy);
-		}
-		return proxy;
-	}
+        proxy = proxyMap.get(serverName);
+        if (proxy == null) {
+            proxy = createProxy(server);
+            proxyMap.put(serverName, proxy);
+        }
+        return proxy;
+    }
 
-	/**
-	 * Retrieves a proxy object by URI
-	 *
-	 * @param serverURI
-	 * @return
-	 * @throws Exception
-	 */
-	public ProtexServerProxy getProxyByServerURI(String serverURI) throws Exception {
-		IDCServer ourServer = null;
-		ProtexServerProxy proxy = null;
-		String hostName = IDCServer.getHostFromURI(serverURI);
-		ourServer = serverMap.get(hostName);
+    /**
+     * Retrieves a proxy object by URI
+     *
+     * @param serverURI
+     * @return
+     * @throws Exception
+     */
+    public ProtexServerProxy getProxyByServerURI(String serverURI) throws Exception {
+        IDCServer ourServer = null;
+        ProtexServerProxy proxy = null;
+        String hostName = IDCServer.getHostFromURI(serverURI);
+        ourServer = serverMap.get(hostName);
 
-		if (ourServer == null)
-			throw new Exception("Could not find server with name: " + ourServer);
-		proxy = proxyMap.get(ourServer.getServerName());
-		if (proxy == null) {
-			proxy = createProxy(ourServer);
-			proxyMap.put(ourServer.getServerName(), proxy);
-		}
+        if (ourServer == null)
+            throw new Exception("Could not find server with name: " + ourServer);
+        proxy = proxyMap.get(ourServer.getServerName());
+        if (proxy == null) {
+            proxy = createProxy(ourServer);
+            proxyMap.put(ourServer.getServerName(), proxy);
+        }
 
-		return proxy;
-	}
+        return proxy;
+    }
 
-	/**
-	 * This provides the list of server objects for the service to manage Populates the internal map for future lookup
-	 */
-	public void setServers(List<IDCServer> servers) {
-		for (IDCServer server : servers) {
-			serverMap.put(server.getServerName(), server);
-		}
-	}
+    /**
+     * This provides the list of server objects for the service to manage Populates the internal map for future lookup
+     */
+    public void setServers(List<IDCServer> servers) {
+        for (IDCServer server : servers) {
+            serverMap.put(server.getServerName(), server);
+        }
+    }
 
-	/**
-	 * Establish a new proxy
-	 *
-	 * @param server
-	 * @return
-	 */
-	private ProtexServerProxy createProxy(IDCServer server) {
-		ProtexServerProxy proxy = null;
-		try {
-			proxy = new ProtexServerProxy(server.getServerURI(), server.getUserName(), server.getPassword());
+    /**
+     * Establish a new proxy
+     *
+     * @param server
+     * @return
+     */
+    private ProtexServerProxy createProxy(IDCServer server) {
+        ProtexServerProxy proxy = null;
+        try {
+            proxy = new ProtexServerProxy(server.getServerURI(), server.getUserName(), server.getPassword());
 
-			proxy.validateCredentials();
+            proxy.validateCredentials();
 
-			server.setLoggedIn(true);
-			log.info("Login successful");
+            server.setLoggedIn(true);
+            log.info("Login successful");
 
-		} catch (Exception e) {
-			log.error("Error logging in: " + e.getMessage());
-			server.setLoginError(e.getCause().getMessage());
-		}
+        } catch (Exception e) {
+            log.error("Error logging in: " + e.getMessage());
+            server.setLoginError(e.getCause().getMessage());
+        }
 
-		return proxy;
-	}
+        return proxy;
+    }
 
-	public Boolean isLoggedIn() {
-		return loggedIn;
-	}
+    public Boolean isLoggedIn() {
+        return loggedIn;
+    }
 }
